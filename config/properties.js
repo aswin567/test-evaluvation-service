39e63054b2ada4b54b7b834a3628efcd04760f73
aswin567 <aswin.kv@siemens.com>
2020-07-24 23:39:19 +0530

    DB link changed

--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,6 @@
 
 module.exports = {
     PORT : 8080,
-    DB : 'mongodb://localhost:27017/test-evaluvation-service',
+    //DB : 'mongodb://localhost:27017/test-evaluvation-service',
+    DB : 'mongodb://heroku_rdgzzqsg:nuour4i74ehu58124d254mjknm@ds159330.mlab.com:59330/heroku_rdgzzqsg'
 }
